76d229438c1243ca10be618f661ea88ef9a9efc3
aman2 <aman20vaishali@gmail.com>
2024-07-30 07:24:32 +0530

    java project first commit

---
+++ src/test/java/com/Aman/JobBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
