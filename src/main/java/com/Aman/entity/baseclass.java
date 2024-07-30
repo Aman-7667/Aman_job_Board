e4049a178746f01f2ea21ad2f4d012695ade6a16
aman2 <aman20vaishali@gmail.com>
2024-07-30 09:20:22 +0530

    java project first commit

---
+++ src/main/java/com/Aman/entity/baseclass.java
@@ -0,0 +1,9 @@
+package com.Aman.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass @Getter @Setter
+public class baseclass {
+}
